8d8ba1bd153d78e7f89c88efa0dd74dd4f2091d8
Alexander Illarionov <ruzinovrun@gmail.com>
2026-02-02 19:04:09 +0100

    implement greetings

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greetUser() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        var scanner = new Scanner(System.in);
+        var name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
