d1728d78c845ca51ad0b8342efa0b203fca2b5a1
Alexander Illarionov <ruzinovrun@gmail.com>
2026-02-07 10:50:23 +0100

    fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,49 +3,56 @@ package hexlet.code;
 public class App {
     public static void main(String[] args) {
         var scanner = new java.util.Scanner(System.in);
-        while (true) {
-            System.out.println("Please enter the game number and press Enter.");
-            System.out.println("1 - Greet");
-            System.out.println("2 - Even");
-            System.out.println("3 - Calc");
-            System.out.println("4 - GCD");
-            System.out.println("5 - Progression");
-            System.out.println("6 - Prime");
-            System.out.println("0 - Exit");
-            System.out.print("Your choice: ");
-
-            if (!scanner.hasNextLine()) {
-                return;
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        
+        var userChoice = scanner.nextLine().trim();
+        switch (userChoice) {
+            case "1" -> {
+                Cli.greetUser();
             }
-            var userChoice = scanner.nextLine().trim();
-            switch (userChoice) {
-                case "1" -> Cli.greetUser();
-                case "2" -> Engine.run(
+            case "2" -> {
+                Engine.run(
                     hexlet.code.games.Even.getRules(),
                     hexlet.code.games.Even.getRounds()
                 );
-                case "3" -> Engine.run(
+            }
+            case "3" -> {
+                Engine.run(
                     hexlet.code.games.Calc.getRules(),
                     hexlet.code.games.Calc.getRounds()
                 );
-                case "4" -> Engine.run(
+            }
+            case "4" -> {
+                Engine.run(
                     hexlet.code.games.Gcd.getRules(),
                     hexlet.code.games.Gcd.getRounds()
                 );
-                case "5" -> Engine.run(
+            }
+            case "5" -> {
+                Engine.run(
                     hexlet.code.games.Progression.getRules(),
                     hexlet.code.games.Progression.getRounds()
                 );
-                case "6" -> Engine.run(
+            }
+            case "6" -> {
+                Engine.run(
                     hexlet.code.games.Prime.getRules(),
                     hexlet.code.games.Prime.getRounds()
                 );
-                case "0" -> {
-                    return;
-                }
-                default -> {
-                    System.out.print("Unknown choice. Try again: ");
-                }
+            }
+            case "0" -> {
+                return;
+            }
+            default -> {
+                System.out.print("Unknown choice. Try again: ");
             }
         }
     }
